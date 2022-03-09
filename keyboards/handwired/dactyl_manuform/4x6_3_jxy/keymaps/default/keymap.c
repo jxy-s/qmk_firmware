cadce7799e8a1e8ce77f6514bd3ca2d9c672fe45
Johnny Shaw <johnny.shaw@live.com>
2022-03-08 23:24:59 -0700

    update dactyl manuform 4x6 3 jxy keymap

--- keyboards/handwired/dactyl_manuform/4x6_3_jxy/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6_3_jxy/keymaps/default/keymap.c
@@ -34,58 +34,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_GRV,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P, KC_BSLS,
         KC_TAB,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                     KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN, KC_QUOT,
-  _LC(KC_LPRN),   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                     KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,_RC(KC_RPRN),
-           _LA(KC_ESC),KC_RGUI,                                                  KC_LBRC,_RA(KC_RBRC),
-                                         TT(1),KC_BSPC,                  KC_SPC,   TT(1),
+       KC_LCTL,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                     KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH, KC_RCTL,
+                   _LA(KC_ESC),KC_LGUI,                                                  KC_LBRC,_RA(KC_RBRC),
+                                         TT(1),KC_BSPC,                   KC_SPC,  TT(1),
                                                     _LS(KC_DEL),_RS(KC_ENT)
     ),
 
     [1] = LAYOUT(
         KC_EQL,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS,
-         TO(0),  TT(2),KC_HOME,KC_PGUP,KC_PGDN,KC_END,                  KC_LEFT, KC_DOWN, KC_UP,KC_RGHT,  TT(2),  TO(0),
-  _LC(KC_PLUS),KC_EXLM,  KC_AT,KC_HASH, KC_DLR,KC_PERC,                  KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN, _RC(KC_UNDS),
-                       _______,_______,                                                    _COPY,_RA(_PSTE),
+         TO(0),  TT(2),KC_HOME,KC_PGUP,KC_PGDN, KC_END,                  KC_LEFT, KC_DOWN, KC_UP,KC_RGHT,  TT(2),   TO(0),
+       _______,KC_EXLM,  KC_AT,KC_HASH, KC_DLR,KC_PERC,                  KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______,
+                       _______,_______,                                                    _COPY,  _PSTE,
                                        _______,_______,                  _______,_______,
                                                         _______,_______
     ),
 
     [2] = LAYOUT(
          KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,                    KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,
-       _______,_______,_______,_______,_______,_______,                  _______,_______,_______,_______,_______,_______,
-       KC_LCTL,KC_NLCK,KC_SLCK,KC_CAPS,KC_PAUS, KC_INS,                  KC_PSCR,_______,_______,_______,_______,KC_RCTL,
+       _______,_______,KC_MS_L,KC_MS_U,KC_MS_D,KC_MS_R,                  _______,_______,_______,_______,_______,_______,
+       _______,KC_NLCK,KC_SLCK,KC_BTN1,KC_BTN2,KC_CAPS,                  KC_PSCR,KC_PAUS, KC_INS,_______,_______,_______,
                        _______,_______,                                                    __CUT,  _UNDO,
                                        _______,_______,                  _______,_______,
                                                         _______,_______
     )
 };
-
-#define _MOD_TAP_FIX(x, y)\
-case x(y):\
-    if (record->tap.count && record->event.pressed)\
-    {\
-        tap_code16(y);\
-        return false;\
-    }\
-    break
-
-bool process_record_user(uint16_t keycode, keyrecord_t* record)
-{
-    switch (keycode)
-    {
-        _MOD_TAP_FIX(_LC, KC_LPRN);
-        _MOD_TAP_FIX(_RC, KC_RPRN);
-
-        _MOD_TAP_FIX(_LA, KC_ESC);
-        _MOD_TAP_FIX(_RA, KC_RBRC);
-
-        _MOD_TAP_FIX(_LC, KC_PLUS);
-        _MOD_TAP_FIX(_RC, KC_UNDS);
-
-        _MOD_TAP_FIX(_RA, _PSTE);
-    }
-
-    return true;
-}
 
 void persistent_default_layer_set(uint16_t default_layer)
 {
