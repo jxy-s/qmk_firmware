d7e4e641adc21ce5825f64d8b9eb8631408b1e04
Johnny Shaw <johnny.shaw@live.com>
2025-06-28 22:24:35 -0600

    fix charybdis 4x6 r-crtl

--- keyboards/bastardkb/charybdis/4x6/keymaps/jxy-s/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/jxy-s/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
         KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LCTL,
+       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RCTL,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                            KC_BSPC, JXY_DEL,    JXY_ENT,  KC_SPC,   LYR_1,
                                                     KC_LALT,    KC_LGUI, KC_RALT
